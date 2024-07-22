9493fe48b6a8e38400436e48086ba3077733fd58
7kEnoLd <1317174703@qq.com>
2024-07-23 00:43:48 +0800

    合并后删除不必要的文件

--- main.py
+++ main.py
@@ -50,51 +50,51 @@ def get_location(city, line, mykey):
 
 
 def main(city):
-    # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50'}
-    # print("爬取中")    #模拟浏览器发送请求
-    # pinyin_list = pypinyin.pinyin(city, style=pypinyin.NORMAL)  # 设置pypinyin的拼音风格为带声调的拼音
-    # pinyin_city = ''.join([item[0] for item in pinyin_list])    #将拼音列表中的每个拼音取出第一个字母，并通过 ''.join() 方法将它们连接成一个字符串
-    # all_url = f'https://{pinyin_city}.8684.cn/'
-    # start_html = requests.get(all_url, headers=headers)   #使用 BeautifulSoup 库将获取到的 HTML 文本进行解析，生成对象 Soup，以便后续对 HTML 结构进行操作。
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50'}
+    print("爬取中")    #模拟浏览器发送请求
+    pinyin_list = pypinyin.pinyin(city, style=pypinyin.NORMAL)  # 设置pypinyin的拼音风格为带声调的拼音
+    pinyin_city = ''.join([item[0] for item in pinyin_list])    #将拼音列表中的每个拼音取出第一个字母，并通过 ''.join() 方法将它们连接成一个字符串
+    all_url = f'https://{pinyin_city}.8684.cn/'
+    start_html = requests.get(all_url, headers=headers)   #使用 BeautifulSoup 库将获取到的 HTML 文本进行解析，生成对象 Soup，以便后续对 HTML 结构进行操作。
+
+    Soup = BeautifulSoup(start_html.text, 'html.parser')
+    all_a = Soup.find('div', class_="bus-layer depth w120").find_all('a')  # <div class="bus-layer depth w120">，然后找到其中的所有 <a> 标签，将它们存储在 all_a 变量中
+    # 过滤出纯数字的结果
+    all_a = [a.get_text() for a in all_a if re.match(r'^\d+$', a.get_text())]
+
+    filename = f"data_{city}.txt"
+
+    # 所有线路
+    # for a in all_a:
+    #     Network_list = []
+    #     html = all_url + '/line' + a #将目标网站的 URL 地址和 href 属性的值拼接起来，构造出一个新的 URL 地址
+    #     second_html = requests.get(html, headers=headers)
+    #     Soup2 = BeautifulSoup(second_html.text, 'html.parser')
+    #     try:
+    #         all_a2 = Soup2.find('div', class_='list clearfix').find_all('a')    #找到 <div class="list clearfix">，然后找到其中的所有 <a> 标签
+    #     except:
+    #         continue
+    #     for a2 in all_a2:
+    #         title1 = a2.get_text()
+    #         Network_list.append(title1)
     #
-    # Soup = BeautifulSoup(start_html.text, 'html.parser')
-    # all_a = Soup.find('div', class_="bus-layer depth w120").find_all('a')  # <div class="bus-layer depth w120">，然后找到其中的所有 <a> 标签，将它们存储在 all_a 变量中
-    # # 过滤出纯数字的结果
-    # all_a = [a.get_text() for a in all_a if re.match(r'^\d+$', a.get_text())]
-    #
-    # filename = f"data_{city}.txt"
-    #
-    # # 所有线路
-    # # for a in all_a:
-    # #     Network_list = []
-    # #     html = all_url + '/line' + a #将目标网站的 URL 地址和 href 属性的值拼接起来，构造出一个新的 URL 地址
-    # #     second_html = requests.get(html, headers=headers)
-    # #     Soup2 = BeautifulSoup(second_html.text, 'html.parser')
-    # #     try:
-    # #         all_a2 = Soup2.find('div', class_='list clearfix').find_all('a')    #找到 <div class="list clearfix">，然后找到其中的所有 <a> 标签
-    # #     except:
-    # #         continue
-    # #     for a2 in all_a2:
-    # #         title1 = a2.get_text()
-    # #         Network_list.append(title1)
-    # #
-    # #     file = open(filename, 'a')
-    # #     file.write(str(Network_list))
-    # #     file.close()
-    #
-    #
-    # Network_list = []
-    # html = all_url + '/line1' #将目标网站的 URL 地址和 href 属性的值拼接起来，构造出一个新的 URL 地址
-    # second_html = requests.get(html, headers=headers)
-    # Soup2 = BeautifulSoup(second_html.text, 'html.parser')
-    # all_a2 = Soup2.find('div', class_='list clearfix').find_all('a')    #找到 <div class="list clearfix">，然后找到其中的所有 <a> 标签
-    # for a2 in all_a2:
-    #     title1 = a2.get_text()
-    #     Network_list.append(title1)
-    #
-    # file = open(filename, 'a')
-    # file.write(str(Network_list))
-    # file.close()
+    #     file = open(filename, 'a')
+    #     file.write(str(Network_list))
+    #     file.close()
+
+
+    Network_list = []
+    html = all_url + '/line1' #将目标网站的 URL 地址和 href 属性的值拼接起来，构造出一个新的 URL 地址
+    second_html = requests.get(html, headers=headers)
+    Soup2 = BeautifulSoup(second_html.text, 'html.parser')
+    all_a2 = Soup2.find('div', class_='list clearfix').find_all('a')    #找到 <div class="list clearfix">，然后找到其中的所有 <a> 标签
+    for a2 in all_a2:
+        title1 = a2.get_text()
+        Network_list.append(title1)
+
+    file = open(filename, 'a')
+    file.write(str(Network_list))
+    file.close()
 
     f = open(f'data_{city}.txt', encoding='gbk')  # 载入的txt文件
     txt = []
