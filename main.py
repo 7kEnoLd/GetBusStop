a102a08d10c9d3eb5c60d36ae42533235498feef
7kEnoLd <1317174703@qq.com>
2024-07-26 22:29:23 +0800

    解决了关键词匹配问题，线路类型选择逻辑问题和部分线路无法一次性全部添加的问题。添加了关键词匹配，首先对含有”/“的多条同名线路进行筛选，其次若没有”路“为其添加”路“，并添加括号判断，最后我们把有无”(临时)“视为同一线路的车，优先判断含有”(临时)“的车，如果没有则只判断没有的。现在循环执行，可以在没查找完的基础上继续查找。优化了线路类型的选择逻辑。

--- main.py
+++ main.py
@@ -8,154 +8,163 @@ import pypinyin
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def get_location(city, line, mykey):
+
+def get_location(city, linename, mykey):
     print("请耐心等待")
-    url_api = 'https://restapi.amap.com/v3/bus/linename?city={}&keywords={}&key={}&extensions=all'.format(city, line, mykey)
+    url_api = 'https://restapi.amap.com/v3/bus/linename?city={}&keywords={}&key={}&extensions=all'.format(city, linename, mykey)
     res = requests.get(url_api).text   #用requests库发送HTTP请求获取文本形式的API的响应数据
     rt = json.loads(res)    #将返回的json字符串转换为字典
-    i = 0
-    # line_name = rt['buslines'][0]['name']
-    # polyline = rt['buslines'][0]['polyline']#经纬度坐标
-    # info = [line_name, polyline]   #包含路径名跟坐标点的列表
-    stop = rt['buslines'][0]['busstops'] #stop 是一个包含车站信息的字典列表，每个字典表示一个车站的信息。
-    # type = rt['buslines'][0]['type']  # 线路类型
-    # name = rt['buslines'][0]['name']  # 线路名称
-    # start_time = rt['buslines'][0]['start_time']  # 开始营运时间
-    # end_time = rt['buslines'][0]['end_time']  # 停止营运时间
-    # time_buy = ''
-    # try:
-    #     time_buy = start_time + '--' + end_time   #计算运行时间
-    # except:
-    #     pass
-    # company = rt['buslines'][0]['company']  # 公交公司
-    # distance = rt['buslines'][0]['distance']  # 总里程
-    # basic_price = rt['buslines'][0]['basic_price']  # 参考票价
-    # form_lines = ""
 
-    # 构造数据结构
-    id = []
-    name = []
-    lineidx = []
-    sequence = []
-    location = []
+    keyword_parts = linename.split('/')  # 筛选同名线路，如832路/通勤832
+    idx = process_buslines(keyword_parts, linename, rt)
 
-    for i in range(len(stop)):
-        id.append(stop[i]['id'])
-        name.append(stop[i]['name'])
-        lineidx.append(line)
-        sequence.append(stop[i]['sequence'])
-        location.append(stop[i]['location'])
+    stop = rt['buslines'][idx]['busstops']  # stop 是一个包含车站信息的字典列表，每部字典表示一个车站的信息。
+
+    return [[stop[i]['id'], stop[i]['name'], linename, stop[i]['sequence'], stop[i]['location']] for i in range(len(stop))]
 
 
-    return [id, name, lineidx, sequence, location]
+def process_buslines(keyword_parts, linename, rt):
+    def match_criteria(name, criteria):
+        base, temp = criteria
+        if '路' not in base[0]:  # 选中第一个分割部分
+            base += '路'
+        if temp:
+            return base[0] + '(' in name and '临时' in name
+        else:
+            return base[0] + '(' in name
 
+    if rt['status'] != '1':
+        print(f"查询失败: {rt['info']}")
+        return None
+
+    buslines = rt.get('buslines', [])
+    if not buslines:
+        print(f"没有找到名称中包含 '{linename}' 的公交线路。")
+        return None
+
+    split_keywords = [part.split('(临时)') if '(临时)' in part else [part, ''] for part in keyword_parts]
+    found = False
+
+    # 优先查找同时包含 base( 和 临时 的结果
+    for index, line in enumerate(buslines):
+        name = line['name']
+        if any(match_criteria(name, [base, True]) for base in split_keywords):
+            return index
+
+    # 如果没有找到同时包含 base( 和 临时 的结果，再查找仅包含 base( 的结果
+    if not found:
+        for index, line in enumerate(buslines):
+            name = line['name']
+            if any(match_criteria(name, [base, False]) for base, _ in split_keywords):
+                return index
+
+    print(f"没有找到名称中包含 '{linename}' 的公交线路。")
+    return None
+
+
+def process_txt_file(file_path):
+    with open(file_path, encoding='gbk') as f:
+        pattern = r"'(.*?)'"
+        data = set()
+        for line in f:
+            matches = re.findall(pattern, line)
+            data.update(matches)
+    return data
+
+
+def load_existing_routes(csv_file):
+    if not os.path.exists(csv_file):
+        return set()
+
+    df_existing = pd.read_csv(csv_file, encoding='utf-8-sig')
+    return set(df_existing['线路编号'].unique())
+
+
+def write_to_csv(csv_file, rows, mode='a', include_header=False):
+    with open(csv_file, mode, encoding='utf-8-sig', newline='') as f:
+        csv_writer = csv.writer(f)
+        if include_header:
+            csv_writer.writerow(["站点ID", "站点名称", "线路编号", "序号", "经纬度"])
+        csv_writer.writerows(rows)
+
+
+def main_conduct(city, mykey, txt_file_path, csv_file_path, processed_lines):
+    # 读取和处理txt文件中的数据
+    data = process_txt_file(txt_file_path)
+
+    # 加载已存在的CSV文件中的路线，避免重复写入
+    existing_routes = load_existing_routes(csv_file_path)
+
+    first_write = not os.path.exists(csv_file_path)
+    rows_to_write = []
+    new_lines_processed = []
+
+    for linename in data:
+        if linename in existing_routes or linename in processed_lines:
+            continue
+
+        time.sleep(1)  # 接口并发量限制是1s/次，防止频繁调用 API 导致 API 服务被封禁
+        try:
+            location_data = get_location(city, linename, mykey)
+            if location_data:
+                # 转置操作
+                # transposed_data = list(zip(*location_data))
+                rows_to_write.extend(location_data)
+                new_lines_processed.append(linename)
+        except Exception as e:
+            print(f"获取 {linename} 的数据时出错: {e}")
+
+    if rows_to_write:
+        write_to_csv(csv_file_path, rows_to_write, mode='a', include_header=first_write)
+    else:
+        print("没有新的数据需要写入。")
+
+    print(f'已完成当前批次处理。')
+    return new_lines_processed
 
 def main(city):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50'}
-    print("爬取中")    #模拟浏览器发送请求
+    filename = get_lines(city)
+    # '9e4e6c67c9df144256d11235d36d210b'
+    mykey = '990981ab890b67b956077f7c081ea433'  # 容量限制，暂时只有一个Key
+
+    processed_lines = []
+    while True:
+        new_lines_processed = main_conduct(city, mykey, filename, f'{city}公交站点.csv', processed_lines)
+        if not new_lines_processed:
+            break
+        processed_lines.extend(new_lines_processed)
+
+    print(f'所有线路处理完成，输出结果保存在：{city}公交站点.csv')
+
+
+def get_lines(city):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50'}
+    print("爬取中")  # 模拟浏览器发送请求
     pinyin_list = pypinyin.pinyin(city, style=pypinyin.NORMAL)  # 设置pypinyin的拼音风格为带声调的拼音
-    pinyin_city = ''.join([item[0] for item in pinyin_list])    #将拼音列表中的每个拼音取出第一个字母，并通过 ''.join() 方法将它们连接成一个字符串
+    pinyin_city = ''.join([item[0] for item in pinyin_list])  # 将拼音列表中的每个拼音取出第一个字母，并通过 ''.join() 方法将它们连接成一个字符串
     all_url = f'https://{pinyin_city}.8684.cn/'
-    start_html = requests.get(all_url, headers=headers)   #使用 BeautifulSoup 库将获取到的 HTML 文本进行解析，生成对象 Soup，以便后续对 HTML 结构进行操作。
-
+    start_html = requests.get(all_url,
+                              headers=headers)  # 使用 BeautifulSoup 库将获取到的 HTML 文本进行解析，生成对象 Soup，以便后续对 HTML 结构进行操作。
     Soup = BeautifulSoup(start_html.text, 'html.parser')
-    all_a = Soup.find('div', class_="bus-layer depth w120").find_all('a')  # <div class="bus-layer depth w120">，然后找到其中的所有 <a> 标签，将它们存储在 all_a 变量中
-    # 过滤出纯数字的结果
-    all_a = [a.get_text() for a in all_a if re.match(r'^\d+$', a.get_text())]
-
+    all_a = Soup.find('div', class_="bus-layer depth w120").find_all(
+        'a')  # <div class="bus-layer depth w120">，然后找到其中的所有 <a> 标签，将它们存储在 all_a 变量中
+    # 查找所有a标签并过滤href属性包含"line"的标签
+    line_links = [a['href'] for a in Soup.find_all('a', href=True) if 'line' in a['href']]
     filename = f"data_{city}.txt"
-
-    # 所有线路
-    # for a in all_a:
-    #     Network_list = []
-    #     html = all_url + '/line' + a #将目标网站的 URL 地址和 href 属性的值拼接起来，构造出一个新的 URL 地址
-    #     second_html = requests.get(html, headers=headers)
-    #     Soup2 = BeautifulSoup(second_html.text, 'html.parser')
-    #     try:
-    #         all_a2 = Soup2.find('div', class_='list clearfix').find_all('a')    #找到 <div class="list clearfix">，然后找到其中的所有 <a> 标签
-    #     except:
-    #         continue
-    #     for a2 in all_a2:
-    #         title1 = a2.get_text()
-    #         Network_list.append(title1)
-    #
-    #     file = open(filename, 'a')
-    #     file.write(str(Network_list))
-    #     file.close()
-
-
     Network_list = []
-    html = all_url + '/line1' #将目标网站的 URL 地址和 href 属性的值拼接起来，构造出一个新的 URL 地址
+    html = all_url + line_links[0]  # 将目标网站的 URL 地址和 href 属性的值拼接起来，构造出一个新的 URL 地址
     second_html = requests.get(html, headers=headers)
     Soup2 = BeautifulSoup(second_html.text, 'html.parser')
-    all_a2 = Soup2.find('div', class_='list clearfix').find_all('a')    #找到 <div class="list clearfix">，然后找到其中的所有 <a> 标签
+    all_a2 = Soup2.find('div', class_='list clearfix').find_all('a')  # 找到 <div class="list clearfix">，然后找到其中的所有 <a> 标签
     for a2 in all_a2:
         title1 = a2.get_text()
         Network_list.append(title1)
-
     file = open(filename, 'a')
     file.write(str(Network_list))
     file.close()
+    return filename
 
-    f = open(f'data_{city}.txt', encoding='gbk')  # 载入的txt文件
-    txt = []
-    for line in f:  # 它将读取文件中的每一行，并将其存储在变量line中。然后，它使用正则表达式r"'(.*?)'
-        word = line          #"来查找每个行中以单引号括起来的子字符串，并将这些子字符串存储在变量matches中。
-    pattern = r"'(.*?)'"     #最后，这个代码片段将匹配到的所有子字符串存储在一个名为data的列表中
-    matches = re.findall(pattern, word)    #findall() 函数，在字符串 word 中查找与给定模式 pattern 匹配的所有子字符串。findall() 函数会返回一个包含所有匹配结果的列表
-    data = list(matches)
-
-    ##########################################################
-    # 查询多余的线路数据
-    # 读取Excel数据
-    file_path = '数据\天津公交站点-剔除未有路线.xlsx'
-    df = pd.read_excel(file_path)
-
-    # 按照线路分组，汇总每条线路的站点名为集合
-    route_stations = df.groupby('路线')['名称'].apply(set)
-
-    # 获取所有线路名称的集合
-    route_names = set(route_stations.index)
-
-    # 转集合进行集合运算
-    data = set(data)
-
-    # 查找data中多余的元素
-    data = list(data - route_names)
-    #############################################################
-
-    a = 0
-    # city_chinese="长沙"
-    f = open(f'{city}公交站点.csv', 'a', encoding='utf-8-sig', newline="")
-    csv_writer = csv.writer(f)
-    # 构建列表头
-    csv_writer.writerow(["站点ID", "站点名称", "线路编号", "序号", "经纬度"])
-    id = []
-    name = []
-    lineidx = []
-    sequence = []
-    location = []
-
-    mykey = '9e4e6c67c9df144256d11235d36d210b'
-
-    for i in data:
-        time.sleep(0.5) #暂停 0.5 秒，防止频繁调用 API 导致 API 服务被封禁!!!
-        a += 1    #已经处理的结果数量
-
-        #if a == 1:  # 加载数量
-        #    break
-        try:
-            line_data = get_location(city, i, mykey)
-            with open(f'{city}公交站点.csv', 'a', newline='') as file:
-                csv_writer = csv.writer(file)
-
-                # 使用zip进行转置操作
-                for row in zip(*line_data):
-                    csv_writer.writerow(row)
-        except:
-            pass
-    print(f'已完成，输出结果保存在：{city}公交站点.csv')
-    f.close()
 
 city = input("输入城市中文名称:")
 csv_filename = f'{city}公交站点.csv'
